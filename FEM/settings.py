4345c24f96168fbd3175c0c308d571201446203c
shubham9803 <shubham.a.bhor@gmail.com>
2026-03-09 00:35:16 +0530

    backend deployment prep

--- FEM/settings.py
+++ FEM/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 AUTH_USER_MODEL = 'accounts.User'
